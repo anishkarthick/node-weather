c57143b55b85e18a4d9dfccaff99706b67a0659e
unknown <sanishkarthicks@gmail.com>
2021-09-01 16:40:26 +0530

    changes in forecast and about

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=90ce4217a56cd7c4532c50e68465f5e0&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=90ce4217a56cd7c4532c50e68465f5e0&query=' + latitude + ',' + longitude + '&units=m'
     request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback('unable to find the weather services', undefined)
@@ -9,8 +9,7 @@ const forecast = (latitude, longitude, callback) => {
             //  console.log(response.body.error)
             callback('unable to find the location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees and it feels like ' + body.current.feelslike + ' degrees')
-
+            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' °C and it feels like ' + body.current.feelslike + '  °C. The localtime is '+ body.location.localtime)
         }
     })
 
